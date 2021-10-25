6a260a81a9372073e75343545c31ded8c7159629
Ethan Bear <ehwbear@icloud.com>
2021-10-25 11:08:33 -0700

    Create initial html

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $("#ft").submit(function(event) {
+    event.preventDefault();
+    const fav1 = $("input#fav1").val();
+    const fav2 = $("input#fav2").val();
+    const fav3 = $("input#fav3").val();
+    const favoriteThings = [];
+    favoriteThings.push(fav1);
+    favoriteThings.push(fav2);
+    favoriteThings.push(fav3);
+
+    $("#output").text(favoriteThings);
+  })
+})
